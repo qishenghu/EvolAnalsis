29867b5f5f9d96818a965b74705ef759a99cbca6
qs <brandonhu0215@yahoo.com>
2026-01-07 15:21:49 +0000

    upload teacher postprocess script

---
+++ postprocess_teacher.sh
@@ -0,0 +1,4 @@
+python scripts/filter_teacher_trajectories.py \
+    --input data/teacher_trajectories/alfworld_qwen72b.jsonl \
+    --output data/teacher_trajectories/alfworld_qwen72b_filtered \
+    --threshold 1.0
